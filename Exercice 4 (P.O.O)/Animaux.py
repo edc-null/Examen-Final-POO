072ba7acdb39874216f2253dfb500dc1e0632a52
edc-null <149556401+edc-null@users.noreply.github.com>
2026-01-15 17:45:09 -0500

    J'ai répondu à la première question posée à l'exercice 4.

--- Exercice 4 (P.O.O)/Animaux.py
+++ Exercice 4 (P.O.O)/Animaux.py
@@ -22,3 +22,19 @@ animal.bruit()
 chien.bruit()
 chat.bruit()
 
+"""
+o	Que veut dire le polymorphisme en python
+
+Le polymorphisme en python fait référence au fait que des objets peuvent utiliser la
+même méthode, mais que le résultat peut varier si le "type" d'objet est différent. C'est
+plus facile à expliquer avec l'exemple ci-dessus. Un chat et un chien sont tout deux des
+animaux. Ils ont certaines propriétés en commun, comme la capacité à faire un bruit.
+Par contre, la capacité à faire un bruit peut "prendre PLUSIEURS FORMES" dépendamment de
+l'animal (produire un son différent).
+
+
+
+
+
+
+"""

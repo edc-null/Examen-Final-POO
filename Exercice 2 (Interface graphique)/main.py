96626314d73d6949398cfa37d3b3851ccc1a3c83
edc-null <149556401+edc-null@users.noreply.github.com>
2026-01-15 16:18:42 -0500

    J'ai créé tout ce qui va avec le UI et son layout

--- Exercice 2 (Interface graphique)/main.py
+++ Exercice 2 (Interface graphique)/main.py
@@ -8,13 +8,37 @@ def main():
     app = QApplication(sys.argv)
 
     fen = QWidget()
-    fen.setGeometry(50, 50, 400, 400)
+    #fen.setGeometry(50, 50, 400, 400)
 
     grid = QGridLayout()
     fen.setLayout(grid)
-    fen.setStyleSheet("background-color:#99ccff;font-family:\"Times New Roman\"")
 
     # Créer et ajouter les autres widgets
+
+    # Labels
+    grid.addWidget(QLabel("Entrer la valeur de N"), 0, 0)
+    grid.addWidget(QLabel("Voici le double:"), 1, 0)
+
+    # LineEdit
+    entier = QLineEdit()
+    double = QLineEdit()
+    double.setReadOnly(True)
+    grid.addWidget(entier, 0, 1)
+    grid.addWidget(double, 1, 1)
+
+    # Boutons
+    btn_valider = QPushButton("Valider l'opération")
+    grid.addWidget(btn_valider, 2, 1)
+    btn_valider.clicked.connect(action_valider)
+
+    btn_sauvegarder = QPushButton("Sauvegarder")
+    grid.addWidget(btn_sauvegarder, 3, 1)
+    btn_sauvegarder.clicked.connect(action_save)
+
+    btn_charger = QPushButton("Charger")
+    grid.addWidget(btn_charger, 3, 0)
+    btn_charger.clicked.connect(action_charger)
+
 
     fen.show()
     app.exec()

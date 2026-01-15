7ede9190232453c0d2e8a818db503f36f50865f8
edc-null <149556401+edc-null@users.noreply.github.com>
2026-01-15 14:57:48 -0500

    J'ai fait la classe Affichage et ses 2 méthodes.
    saisie_utilisateur permet d'obtenir l'entier n entré par l'utilisateur, et print_triangles crée les deux triangles et les affiche.

--- Exercice 1 (Triangles)/Affichage.py
+++ Exercice 1 (Triangles)/Affichage.py
@@ -1,10 +1,32 @@
-
+from Triangle import Triangle
 class Affichage:
 
-    def saisie_utilisateur:
+    def saisie_utilisateur(self):
         # Boucle de saisie qui va s'assurer d'avoir un nombre entier n comme input
+        while True:
+            try:
+                n = int(input("Veuillez saisir un nombre entier: "))
+                if n >= 0:
+                    return n
+                else:
+                    print("Erreur: le nombre doit être positif.")
+            except ValueError:
+                print("Erreur. Veuillez saisir un nombre entier.")
 
-    def print_triangles:
+
+    def print_triangles(self):
+        n = self.saisie_utilisateur()
+        triangle_gauche = Triangle(n, True)
+        triangle_droite = Triangle(n, False)
+
+        lignes_gauche = triangle_gauche.return_lines()
+        lignes_droite = triangle_droite.return_lines()
+
+        print() #Skip une ligne
+        for i in range(n):
+            print(lignes_gauche[i] + "  " + lignes_droite[i])
+
+
 
 
 

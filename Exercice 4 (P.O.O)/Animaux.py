d494b1a79449b7e8bb548a298a074eec32cd58c2
edc-null <149556401+edc-null@users.noreply.github.com>
2026-01-15 17:32:05 -0500

    J'ai choisi les chiens et les chats et le concept plus général d'animal pour illustrer des classes, de l'héritage (nom) et du polymorphisme (la méthode bruit, qui est différente selon l'animal, mais tous les animaux font un bruit).

---
+++ Exercice 4 (P.O.O)/Animaux.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, nom, poids):
+        self.nom = nom
+
+    def bruit(self):
+
+class Chien(Animal):
+    def bruit(self):
+
+class Chat(Animal):
+    def bruit(self):
+
+
